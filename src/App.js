c96a844da27e6ebe457d6c9d22354d20b007f758
LEVINSKY Nicolas <nicolasdavidlevinsky@gmail.com>
2022-08-30 19:50:03 -0300

    Subida del navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './media/tiendaLogo.png'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const app = ( ) =>{
+
+  return(
+    <header>
+      <div className="contentNavBar">
+
+      <img class="logoNav" src={logo} alt="Logo" />
+
+
+        <ul className="items">
+          <li><a href="">Inicio</a></li>
+          <li><a href="">Categorias</a></li>
+          <li><a href="">Ofertas</a></li>
+          <li><a href="">Lo mas nuevo</a></li>
+        </ul>
+      </div>
+    </header>
+  )
 }
 
-export default App;
+
+export default app
