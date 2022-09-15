e567a4c0bc1816001fbc9c97c0a205a73a4cd60e
LEVINSKY Nicolas <nicolasdavidlevinsky@gmail.com>
2022-09-14 22:11:20 -0300

    React-doom error

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import NavBar from '../Components/NavBar'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import ItemListContainer from '../Contenedores/ItemListContainer'
 import ItemDetailContainer from '../Contenedores/ItemDetailContainer'
 
@@ -6,17 +7,19 @@ import ItemDetailContainer from '../Contenedores/ItemDetailContainer'
 
 const Home = () =>{
   return(
-    <div>
-      <NavBar/>
-      <ItemDetailContainer />
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Route patch='/' > <ItemDetailContainer /> </Route>
+      <Route patch='/category/:id'> <p>Hola1</p> </Route>
+      <Route patch='/item/:id'> <p>Hola2</p> </Route>
+    </Switch>
+     { /*<NavBar/> */}
+    </BrowserRouter>
   )
 }
 /* Aca abajo se llama al stock, despues del desafio lo vuelvo a subir
 
-      <div className='contentProducto'>
-      <ItemListContainer />
-      </div>
+      <div className='contentProducto'> <ItemListContainer /> </div>
 
 */
 
